e11dc1658ccdea7b0c5f81bd41a6185c7ce53742
Gyan Prabhat <gyanprabhat07@gmail.com>
2024-01-16 01:17:38 +0530

    initial

---
+++ src/config/footer.js
@@ -0,0 +1,81 @@
+export const footerSocials = [
+	{
+		name: 'Twitter',
+		url: 'https://twitter.com/gyanprabhat_',
+		icon: 'mdi:twitter',
+	},
+	{
+		name: 'Youtube',
+		url: 'https://youtube.com/c/ToxicElement7',
+		icon: 'mdi:youtube',
+	},
+	{
+		name: 'Github',
+		url: 'https://github.com/gyanprabhat7',
+		icon: 'mdi:github',
+	},
+	{
+		name: 'Discord',
+		url: 'https://discord.gg/AQCXAQVZeY',
+		icon: 'ic:baseline-discord',
+	},
+	{
+		name: 'Instagram',
+		url: 'https://instagram.com/gyann.co',
+		icon: 'mdi:instagram',
+	},
+];
+
+export const footerLists = [
+	{
+		title: 'Ventures',
+		items: [
+			{
+				title: 'Epiction Interactive',
+				slug: 'https://epiction.co',
+			},
+			{
+				title: 'Go Digital',
+				slug: 'https://godigital.epiction.co',
+			},
+			{
+				title: 'Mangatize Apparels',
+				slug: 'https://goto.mangatize.shop',
+			},
+			{
+				title: 'Coders Potion',
+				slug: 'https://coderspotion.company.site',
+			},
+			{
+				title: 'Baroda Bucket List',
+				slug: 'https://rebrand.ly/yettobepublished',
+			},
+		],
+	},
+	{
+		title: 'Directory',
+		items: [
+			{
+				title: 'About',
+				slug: '/company/about',
+			},
+			{
+				title: 'Blog',
+				slug: '/blog',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	},
+	{
+		title: 'Courses',
+		items: [
+			{
+				title: 'Coming Soon',
+				slug: '#',
+			},
+		],
+	},
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Gyann | Student & Techpreneur`,
+	description: `Gyan Prabhat (founder Epiction Interactive) personal website.`,
+	url: `https://gyan.epiction.co`, // No trailing slash!
+	name: `Gyan`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: true,
+	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
